4fa1f4a6b1139c053f51c49df1a07073cee40fb1
quiet-honey <skap0329@naver.com>
2023-12-14 03:54:05 +0900

    docs: 곰터뷰와 IDrive e2 (3) - AWS S3 SDK for JavaScript를 활용한 IDrive e2 사용법

--- meta-config.js
+++ meta-config.js
@@ -1,33 +1,34 @@
 module.exports = {
-	teamName: "Team NDD Blog", // TeamName을 기입합니다.
-	// TeamBlog에 대한 간단한 설명을 기입합니다.
-	teamDescription: "Team NDD Blog 입니다. 여러분의 방문을 환영합니다☺️",
-	githubLink: "https://github.com/adultlee/team-blog-starter/tree/main", // Team에서 함께 관리하는 Github Link를 작성합니다.
-	members: [
-		{
-			name: "adultlee",
-			description: "안녕하세요! 팀 NDD의 바지사장을 맡고 있습니다!",
-			githubLink: "https://github.com/adultlee",
-		},
-		{
-			name: "Yoon-Hae-Min",
-			description: "프론트엔드 개발자 윤해민입니다.",
-			githubLink: "https://github.com/Yoon-Hae-Min",
-		},
-		{
-			name: "milk717",
-			description: "I'm 김수민",
-			githubLink: "https://github.com/milk717",
-		},
-		{
-			name: "JangAJang",
-			description: "I'm 이장희",
-			githubLink: "https://github.com/JangAJang",
-		},
-		{
-			name: "quiet-honey",
-			description: "I'm 조용헌",
-			githubLink: "https://github.com/quiet-honey",
-		},
-	],
+  teamName: "Team NDD Blog", // TeamName을 기입합니다.
+  // TeamBlog에 대한 간단한 설명을 기입합니다.
+  teamDescription: "Team NDD Blog 입니다. 여러분의 방문을 환영합니다☺️",
+  githubLink: "https://github.com/adultlee/team-blog-starter/tree/main", // Team에서 함께 관리하는 Github Link를 작성합니다.
+  members: [
+    {
+      name: "adultlee",
+      description: "안녕하세요! 팀 NDD의 바지사장을 맡고 있습니다!",
+      githubLink: "https://github.com/adultlee",
+    },
+    {
+      name: "Yoon-Hae-Min",
+      description: "프론트엔드 개발자 윤해민입니다.",
+      githubLink: "https://github.com/Yoon-Hae-Min",
+    },
+    {
+      name: "milk717",
+      description: "I'm 김수민",
+      githubLink: "https://github.com/milk717",
+    },
+    {
+      name: "JangAJang",
+      description: "I'm 이장희",
+      githubLink: "https://github.com/JangAJang",
+    },
+    {
+      name: "quiet-honey",
+      description:
+        "매일을 후회없이 살아가기 위해 노력하는 백엔드 개발자입니다!",
+      githubLink: "https://github.com/quiet-honey",
+    },
+  ],
 };
